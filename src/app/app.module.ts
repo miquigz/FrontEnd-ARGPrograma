b937bfd3ba0e379563ebad114d72a2c64f0a9d6d
Miqueas Gimenez <miquigz02@gmail.com>
2022-08-25 10:45:40 -0300

    primeros cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { EstudiosComponent } from './components/estudios/estudios.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    PerfilComponent,
+    EstudiosComponent,
+    HabilidadesComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
